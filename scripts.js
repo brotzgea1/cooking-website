6e25810ff6b69c01a99159978e46e54874b12f9a
Andrew <brotzgea1@gmail.com>
2020-07-29 17:57:18 -0400

    computer layout and fixed card javascript

--- scripts.js
+++ scripts.js
@@ -29,7 +29,7 @@ function ToggleNavItems(){
 	};
 };
 
-
+// /////////Recipe Card/////////
 
 function ToggleCardInfo(){
 	let cardItems = document.getElementsByClassName("card-info");
@@ -39,18 +39,42 @@ function ToggleCardInfo(){
 	expandCard();
 };
 
+function ToggleCardInfoOff(){
+	let cardItems = document.getElementsByClassName("card-info");
+	for(let i=0; i < cardItems.length; i++){
+		cardItems[i].classList.toggle("visible-card-info");
+	};
+	collapseCard();
+};
+
 function expandCard(){
 	let card = document.getElementsByClassName("card");
+	let icon = document.getElementsByClassName("fas fa-arrow-circle-down");
+	let icon2 = document.getElementsByClassName("fas fa-arrow-circle-up");
 	for(let i=0; i < card.length; i++){
-		card[i].style.width = "90%";
-		}
+		card[i].style.width = "600px";
+		};
+	for(let i=0; i < icon.length; i++){
+		icon[i].style.visibility = "hidden";
+		};
+	for(let i=0; i < icon2.length; i++){
+		icon2[i].style.visibility = "visible";
+		};
 };
 
 function collapseCard(){
 	let card = document.getElementsByClassName("card");
+	let icon = document.getElementsByClassName("fas fa-arrow-circle-down");
+	let icon2 = document.getElementsByClassName("fas fa-arrow-circle-up");
 	for(let i=0; i < card.length; i++){
-		card[i].style.width = "20%";
-	}
+		card[i].style.width = "300px";
+	};
+	for(let i=0; i < icon.length; i++){
+		icon[i].style.visibility = "visible";
+		};
+	for(let i=0; i < icon2.length; i++){
+		icon2[i].style.visibility = "hidden";
+		};
 	console.log("hello");
 };
 
